00038023b13710cd9753c352d7d10fb2669b3132
Aiden Kiss <aidenk.7103@gmail.com>
2024-10-17 17:27:15 -0400

    fixed charset big issue

--- setup.sql
+++ setup.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS passwords;
 
-CREATE DATABASE 'passwords' DEFAULT CHARCTER SET utf8mb44 COLLATE utf8mb4_bin;
+CREATE DATABASE 'passwords' DEFAULT CHARCTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 USE passwords;
 
