8b3c5f6d04a6d53bfad2bc26a4ad21873ad6e402
Aiden Kiss <aidenk.7103@gmail.com>
2024-10-17 20:23:27 -0400

    tidying up whitespace/neantess

--- commands.sql
+++ commands.sql
@@ -12,37 +12,29 @@ VALUES
 
 
 --command 2 - get password associated with one of the urls
-SELECT
-    CAST(AES_DECRYPT(account_info.account_password, @key_str, @init_vector) AS CHAR) AS decrypted_password
-FROM
-    account_info
-JOIN
-    website
-ON
-    account_info.id = website.id
-WHERE
-    website.site_url = "https://www.microsoft.com/en-us/";
+SELECT CAST(AES_DECRYPT(account_info.account_password, @key_str, @init_vector) AS CHAR) AS decrypted_password
+FROM account_info
+JOIN website
+ON account_info.id = website.id
+WHERE website.site_url = "https://www.microsoft.com/en-us/";
 
 --command 3 - get all password related data with urls with https (all of mine are https so theres more than 2)
 SELECT
-    website.website AS website_name,
-    website.site_url,
-    account_info.account_username,
-    CAST(AES_DECRYPT(account_info.account_password, @key_str, @init_vector) AS CHAR) AS decrypted_password,
-    account_info.comment,
-    account_info.account_creation
-FROM
-    account_info
-JOIN
-    website
-ON
-    account_info.id = website.id
-WHERE
-    website.site_url LIKE '%https%';
+  website.website AS website_name,
+  website.site_url,
+  account_info.account_username,
+  CAST(AES_DECRYPT(account_info.account_password, @key_str, @init_vector) AS CHAR) AS decrypted_password,
+  account_info.comment,
+  account_info.account_creation
+FROM account_info
+JOIN website
+ON account_info.id = website.id
+WHERE website.site_url LIKE '%https%';
 
 --command 4 - change a url, matching password
 UPDATE website
-JOIN account_info ON website.id = account_info.id
+JOIN account_info
+ON website.id = account_info.id
 SET website.site_url = 'https://www.command4-newgoogle-url.com'
 WHERE CAST(AES_DECRYPT(account_info.account_password, @key_str, @init_vector) AS CHAR) = 'G00Gl3';
 
